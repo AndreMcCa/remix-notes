abac198445c84fcebab4b0e1d6499b8c09218845
ANDRII VAKHOTSKYI <mr.andremcca@gmail.com>
2024-10-03 12:30:31 +0300

    Added form handling logic and note saving

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,8 @@
+import { redirect } from "@remix-run/node";
+
 import NewNote, { links as newNoteLinks } from "../components/NewNote/NewNote";
+
+import { getStoredNotes, storeNotes } from "../data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +10,20 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+export async function action(data) {
+  const { request } = data;
+  const formData = await request.formData();
+
+  const noteData = Object.fromEntries(formData);
+  noteData.id = new Date().toISOString();
+  // Add validation ...
+
+  const existingNotes = await getStoredNotes();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
 }
 
 export function links() {
